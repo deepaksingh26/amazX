183745d1ad4e079dedbc5846a77b9757cc2a7d65
deepaksingh26 <deepakthakurnita@gmail.com>
2025-02-16 19:32:57 +0530

    admin changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const corsOptions = {
         }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
 }
 app.use(cors(corsOptions));
 if(process.env.NODE_ENV==="production"){
